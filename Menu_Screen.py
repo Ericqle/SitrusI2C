aba817677d54bf72792b36c7bf05b48b3a852520
Ericqle <eqle22@gmail.com>
2019-02-07 15:52:11 -0800

    Tabbed Panel for Registers Added

--- Menu_Screen.py
+++ Menu_Screen.py
@@ -1,5 +1,21 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.tabbedpanel import TabbedPanelItem
 
 
 class MenuScreen(Screen):
+
+    tabs = 4
+
+    def swap_to_i2c_screen(self):
+        i2c_screen = self.manager.get_screen("i2c_screen")
+
+        i2c_screen.i2c_tabbed_panel.clear_widgets()
+        i2c_screen.i2c_tabbed_panel.clear_tabs()
+
+        for i in range(0, self.tabs):
+            new_tab = TabbedPanelItem(text="Tab: " + str(i))
+            i2c_screen.i2c_tabbed_panel.add_widget(new_tab)
+
+        self.manager.current = "i2c_screen"
+
     pass
