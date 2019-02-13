66df9665bdd5ba0f1376fad10998fd094cf4bd58
Ericqle <eqle22@gmail.com>
2019-02-13 15:10:17 -0800

    Recycle View for Tabs added

--- Menu_Screen.py
+++ Menu_Screen.py
@@ -1,9 +1,38 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.tabbedpanel import TabbedPanelItem
+from kivy.lang import Builder
+
+Builder.load_string('''
+<I2cRecycleViewRow@BoxLayout>:
+    orientation: 'horizontal'
+    text : ''
+    Label:
+        text: root.text
+    Button:
+        text: 'Show'
+        on_press: print("hi")
+
+<I2cRecycleView@RecycleView>:
+    viewclass: 'I2cRecycleViewRow'
+    RecycleBoxLayout:
+        default_size: None, dp(56)
+        default_size_hint: 1, None
+        size_hint_y: None
+        height: self.minimum_height
+        orientation: 'vertical'        
+
+<I2cTabbedPanelItem>:
+    i2c_recycle_View: i2c_recycle_View
+    I2cRecycleView:
+        id: i2c_recycle_View
+                    ''')
+
+
+class I2cTabbedPanelItem(TabbedPanelItem):
+    pass
 
 
 class MenuScreen(Screen):
-
     tabs = 4
 
     def swap_to_i2c_screen(self):
@@ -13,9 +42,8 @@ class MenuScreen(Screen):
         i2c_screen.i2c_tabbed_panel.clear_tabs()
 
         for i in range(0, self.tabs):
-            new_tab = TabbedPanelItem(text="Tab: " + str(i))
+            new_tab = I2cTabbedPanelItem(text="Tab: " + str(i))
+            new_tab.i2c_recycle_View.data = [{'text': "Button " + str(x), 'id': str(x)} for x in range(30)]
             i2c_screen.i2c_tabbed_panel.add_widget(new_tab)
 
         self.manager.current = "i2c_screen"
-
-    pass
