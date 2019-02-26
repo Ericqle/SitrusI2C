1a079be2d607690cfd8337e07e627cdc9bcda063
Ericqle <eqle22@gmail.com>
2019-02-26 14:37:10 -0800

    Script Outline Added

--- I2C_Screen.py
+++ I2C_Screen.py
@@ -4,6 +4,16 @@ from kivy.factory import Factory
 
 class I2CScreen(Screen):
 
-    def write(self):
+    @staticmethod
+    def write():
         Factory.I2cWritePopup().open()
+
+    @staticmethod
+    def open_load_script():
+        Factory.I2cLoadScriptPopup().open()
+
+    @staticmethod
+    def open_run_script():
+        Factory.I2cRunScriptPopup().open()
+        pass
 
